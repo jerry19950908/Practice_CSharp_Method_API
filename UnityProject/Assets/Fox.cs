23e3f32c027d75957ae861dfe872bdfdab7dfb4c
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2019-11-21 18:16:32 +0800

    作業

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(0f, 10f)]
+    public float speed = 2f;
+
+    public Transform fox;
+
+    private void Move()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+
+        fox.Translate(speed, 0, 0);
+
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+}
